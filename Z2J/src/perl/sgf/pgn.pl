1abfe0b9a98cac8281364e739968dc2b5f33f1eb
GlukKazan <glukkazan@gmail.com>
2017-03-12 16:23:50 +0300

    +

---
+++ Z2J/src/perl/sgf/pgn.pl
@@ -0,0 +1,37 @@
+my %tree;
+
+while (<>) {
+  my $node = \%tree;
+  while (/(\S+)/g) {
+     my $move = $1;
+     if ($move !~ /\d+\./) {
+        if (!$node->{$move}) {
+           $node->{$move} = {};
+        }
+        $node = $node->{$move};
+     }
+  }
+}
+
+sub out {
+  my ($p, $node) = @_;
+  my $n = keys %$node;
+  foreach my $key (keys %$node) {
+     if ($n > 1) {
+         print "(";
+     }
+     print ";$p\[$key\]";
+     my $e = 'W';
+     if ($p eq $e) {
+        $e = 'B';
+     }
+     out($e, $node->{$key});
+     if ($n > 1) {
+         print ")";
+     }
+  }
+}
+
+print "(";
+out('W', \%tree);
+print ")\n";
